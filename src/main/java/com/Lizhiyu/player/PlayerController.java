b3b730bcf53553b5c477d7d2b1c9292d7844f4d3
MrPeach <767130703@qq.com>
2017-07-28 20:18:55 +0800

    commit 3:完成了增加一个球员的接口，在根据id查询球员接口处有一个error。

--- src/main/java/com/Lizhiyu/player/PlayerController.java
+++ src/main/java/com/Lizhiyu/player/PlayerController.java
@@ -1,8 +1,7 @@
 package com.Lizhiyu.player;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,8 +14,42 @@ public class PlayerController {
 	@Autowired
 	private PlayerRepository playerRepository;
 
+
+	/**
+	 * 查询所有球员列表
+	 * @return
+	 */
 	@GetMapping(value = "/players")
 	public List<Player> PlayerList() {
 		return playerRepository.findAll();
 	}
+
+	/**
+	 * 添加一个球员
+	 * @param id
+	 * @param name
+	 * @param club
+	 * @return 球员对象
+	 */
+	@PostMapping(value = "/players")
+	public Player playerAdd(@RequestParam("id") Integer id, @RequestParam("name") String name, @RequestParam("club") String club) {
+		Player player = new Player();
+		player.setId(id);
+		player.setClub(club);
+		player.setName(name);
+
+		 return playerRepository.save(player);
+	}
+
+	//根据id查询一个球员
+	@GetMapping(value = "/players/{id}")
+	public Player playerFindOne(@PathVariable("id") Integer id) {
+		return playerRepository.findOne(id);
+	}
+
+	//更新
+	/*@PutMapping(value = "/players/{id}")
+	//删除
+	@DeleteMapping(value = "/players/{id}")*/
+
 }
