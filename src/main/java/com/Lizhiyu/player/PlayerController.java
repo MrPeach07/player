4f0ae99d0b98a28edab912480b10e3559acd8f94
MrPeach <767130703@qq.com>
2017-07-30 11:51:15 +0800

    commit 5：新增了根据club字段查询球员信息列表的接口。jpa部分实现完成。

--- src/main/java/com/Lizhiyu/player/PlayerController.java
+++ src/main/java/com/Lizhiyu/player/PlayerController.java
@@ -2,8 +2,11 @@ package com.Lizhiyu.player;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import com.Lizhiyu.player.Player;
+import com.Lizhiyu.player.PlayerRepository;
 
 import java.util.List;
+
 
 /**
  * Created by lzy on 17-7-28.
@@ -32,24 +35,65 @@ public class PlayerController {
 	 * @return 球员对象
 	 */
 	@PostMapping(value = "/players")
-	public Player playerAdd(@RequestParam("id") Integer id, @RequestParam("name") String name, @RequestParam("club") String club) {
+	public Player playerAdd(@RequestParam("id") Integer id,
+							@RequestParam("name") String name,
+							@RequestParam("club") String club) {
 		Player player = new Player();
 		player.setId(id);
 		player.setClub(club);
 		player.setName(name);
 
-		 return playerRepository.save(player);
+		return playerRepository.save(player);
 	}
 
-	//根据id查询一个球员
+
+	/**
+	 * 通过id查询球员信息
+	 * @param id
+	 * @return
+	 */
 	@GetMapping(value = "/players/{id}")
-	public Player playerFindOne(@PathVariable("id") Integer id) {
-		return playerRepository.findOne(id);
+	public Player playerGetOne(@PathVariable("id") Integer id) {
+		return playerRepository.getOne(id);
 	}
 
-	//更新
-	/*@PutMapping(value = "/players/{id}")
-	//删除
-	@DeleteMapping(value = "/players/{id}")*/
+	/**
+	 * 更新球员
+	 * @param id
+	 * @param name
+	 * @param club
+	 * @return
+	 */
+	@PutMapping(value = "/players/{id}")
+	public Player playerUpdate(@PathVariable("id") Integer id,
+							 @RequestParam("name") String name,
+							 @RequestParam("club") String club) {
+		Player player = new Player();
+		player.setId(id);
+		player.setName(name);
+		player.setClub(club);
+
+		return playerRepository.save(player);
+	}
+
+	/**
+	 * 根据id删除球员信息
+	 * @param id
+	 */
+	@DeleteMapping(value = "/players/{id}")
+	public void playersDelete(@PathVariable("id") Integer id) {
+		playerRepository.deleteById(id);
+	}
+
+
+	/**
+	 * 根据club查询球员信息
+	 * @param club
+	 * @return
+	 */
+	@GetMapping(value = "/players/club/{club}")
+	public List<Player> playerListByClub(@PathVariable("club") String club) {
+		return playerRepository.findByClub(club);
+	}
 
 }
