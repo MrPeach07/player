ac82e3e5d8d952c315a71b413902d0b230e0f45d
MrPeach <767130703@qq.com>
2017-08-01 20:57:10 +0800

    commit 12：完成了自定义异常类的编写及调试。

--- src/main/java/com/Lizhiyu/player/service/PlayerService.java
+++ src/main/java/com/Lizhiyu/player/service/PlayerService.java
@@ -1,5 +1,8 @@
-package com.Lizhiyu.player;
+package com.Lizhiyu.player.service;
 
+import com.Lizhiyu.player.exception.PlayerException;
+import com.Lizhiyu.player.repository.PlayerRepository;
+import com.Lizhiyu.player.domain.Player;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,12 +22,28 @@ public class PlayerService {
 		playerA.setId(9);
 		playerA.setName("Benzema");
 		playerA.setClub("RealMadrid");
+		playerA.setAge(25);
 		playerRepository.save(playerA);
 
 		Player playerB = new Player();
 		playerB.setId(10);
 		playerB.setName("Messi");
 		playerB.setClub("Barcelona");
+		playerB.setAge(28);
 		playerRepository.save(playerB);
+	}
+
+	public void getAge(Integer id) throws Exception{
+		Player player = playerRepository.getOne(id);
+		Integer age = player.getAge();
+		if(age<13) {
+			//返回球员还在青训期间,code = 101
+			throw new PlayerException(101,"球员还在青训期间");
+		}
+		else if(age > 13 &&age < 18){
+			//返回球员在国家联赛踢球了,code = 100
+			throw new PlayerException(100,"球员在国家联赛踢球了");
+		}
+
 	}
 }
